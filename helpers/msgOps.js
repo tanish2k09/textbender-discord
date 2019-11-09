e51e1f65b159a6c57b91997a344997db76583b40
Tanish Manku <tanish2k09.dev@gmail.com>
2019-11-08 21:22:46 -0800

    Textbender bringup, v1.0.0
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>

---
+++ helpers/msgOps.js
@@ -0,0 +1,188 @@
+module.exports = {
+    nonsense: function (bot, ID, userID) {
+        
+        var msg = 'Hey <@!' + userID + '> ';
+        
+        if (userID == '328440224130793472')
+            msg += 'UwU daddy-san I\'m sorry I can\'t quite understand';
+        else
+            msg += 'you little shit, use ;helpme';
+        
+        bot.sendMessage({
+            to: ID,
+            message: msg
+        });
+    },
+    
+    blacklisted: function (bot, ID, userID) {
+        bot.sendMessage({
+            to: ID,
+            message: 'Hey <@!' + userID + '> you little shit, you\'re blacklisted'
+        });
+    },
+
+    post: function (bot, ID, msg) {
+        if (ID == null || msg == null)
+            return;
+
+        bot.sendMessage({
+            to: ID,
+            message: msg
+        });
+    },
+
+    pingResponder: function (bot, ID) {
+        bot.sendMessage({
+            to: ID,
+            message: '***Jörmungandr*** *would like to know your location!*'
+        });
+    },
+
+    getCommandDescriptions: function () {
+        desc = ";biggspace - Same as ;space but with more spacing probability\n";
+        desc += ";biggspacew - Same as ;spacew but with more spacing probability\n";
+        desc += ";biggspacel - Same as ;spacel but with more spacing probability\n";
+        desc += ";fuck - replies 'you.'\n";
+        desc += ";helpme - DMs you a helpful message about the bot, just like this one. I just became self-aware didn't I?\n";
+        desc += ";inv message - Inverts the message's case\n"
+        desc += ";invl - Inverts the last message's case\n"
+        desc += ";low message - Converts the message into lower case\n"
+        desc += ";lowl - Converts the last message into lower case\n"
+        desc += ";mock message - Outputs a spongebob-mock version of message with random capped alphabets\n";
+        desc += ";mockl - Outputs a ;mock of the last message\n";
+        desc += ";mocklast - Same as ;mockl\n";
+        desc += ";muda - Sends a long MUDA string\n";
+        desc += ";nice - Sends a Nice string to the nice channel with your user tag\n";
+        desc += ";ora - Sends a long ORA string\n";
+        desc += ";ping - Outputs a string to let you know if the bot is up and active\n";
+        desc += ";quoteme message - Puts the message in the just-quotes channel (with wrapped quotation and a credit tag)\n";
+        desc += ";spam message - Spams the 'message' at max 10 times ONLY if you have dev access\n";
+        desc += ";tananana - Imitates the Illuminati sound in... text... yea idk\n"
+        desc += ";upp message - Converts the message into upper case\n"
+        desc += ";uppl - Converts the last message into upper case\n"
+        desc += "\n\nBot built by <@!328440224130793472>";
+        return desc;
+    },
+
+    helpResponder: function (bot, ID) {
+        bot.sendMessage({
+            to: ID,
+            message: 'Hey there! The bot command prefix is \';\'\n\nCommands (case insensitive):\n' + this.getCommandDescriptions()
+        });
+    },
+
+    getCmd: function (message) {
+        if (message.length < 2) {
+            return null;
+        }
+        
+        if (message.indexOf(' ') == -1)
+            return message.substring(1, message.length);
+
+        return message.substring(1, message.indexOf(' '));
+    },
+
+    getArg: function (message, cmdLength) {
+        return message.substring(cmdLength, message.length);
+    },
+
+    postLastMsgMocked: function (bot, event, bender) {
+        var msgOp = this;
+        bot.getMessages({
+            channelID: event.d.channel_id,
+            before: event.d.id,
+            limit: 1
+        }, function (err, res) {
+            msgOp.post(bot, event.d.channel_id, bender.getSpongebobString(res[0].content));
+        });
+    },
+    
+    postLastMsgCased: function (bot, event, bender, type) {
+        var msgOp = this;
+        bot.getMessages({
+            channelID: event.d.channel_id,
+            before: event.d.id,
+            limit: 1
+        }, function (err, res) {
+            msgOp.post(bot, event.d.channel_id, bender.getCasedString(res[0].content, type));
+        });
+    },
+
+    spamPost: function (bot, id, message) {
+        var i;
+        for (i = 0; i < 10; ++i) {
+            this.post(bot, id, message);
+        }
+    },
+
+    weakSpamPost: function (bot, id) {
+        this.post(bot, id, "Lmao this dude thinks he can exploit me to spam, I'm fresh outta fucks to give");
+    },
+
+    postNice: function (bot, userID) {
+        this.post(bot, '624836797045801008', "Nice - <@!" + userID + '>');
+    },
+
+    postOra: function (bot, id) {
+        this.post(bot, id, "ORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORAORA");
+    },
+
+    postMuda: function (bot, id) {
+        this.post(bot, id, "MUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDA");
+    },
+    
+    postQuote: function (bot, message, userID) {
+        var quote = this.getArg(message, 8);
+        
+        if (quote == null || quote.length == 0)
+            return;
+        
+        quote.trim();
+
+        if (quote.length == 0)
+            return;
+
+        this.post(bot, '560626863731507236', '"' + quote + "\"\n- <@!" + userID + ">");
+    },
+    
+    postBruh: function (bot, userID) {
+        this.post(bot, '631528865134673930', "*Ladies and gentlemen, a* ***bruh moment*** *has been declared by <@" + userID + ">*");
+    },
+    
+    postF: function (bot, channelID, symbol) {
+        fText = symbol.repeat(5) + '\n' + symbol.repeat(3) + ('\n' + symbol).repeat(2);
+        this.post(bot, channelID, fText);
+    },
+    
+    killResponder: function (bot, channelID, userID) {
+        this.post(bot, channelID, "Look <@" + userID + ">, I understand what you be goin thru but it do be like that sometimes");
+    },
+    
+    sendHelp: function (bot, event) {
+        userID = event.d.content.split('@')[1];
+        userID = userID.split('>')[0];
+        if (userID.charAt(0) == '!')
+            userID = userID.substring(1, userID.length);
+        
+        this.helpResponder(bot, userID);
+    },
+    
+    postMsgSpaced: function (bot, channelID, bender, cmd, msg) {
+        message = this.getArg(msg, cmd.length + 1);
+        if (message == null)
+            return this.nonsense(bot, channelID);
+        
+        this.post(bot, channelID, bender.getSpacedMsg(message, cmd));
+    },
+    
+    postLastMsgSpaced: function (bot, event, bender, cmd) {
+        var msgOp = this;
+        bot.getMessages({
+            channelID: event.d.channel_id,
+            before: event.d.id,
+            limit: 1
+        }, function (err, res) {
+            msgOp.post(bot, event.d.channel_id, bender.getSpacedMsg(res[0].content, cmd));
+        });
+    },
+};
---
+++ test.js
@@ -0,0 +1,252 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+var msgOp = require('./helpers/msgOps.js');
+var bend = require('./helpers/bender.js');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+var blacklistRetrack = false;
+var actDumb = false;
+var selfID = '629837107451527188';
+var quantumID = '328440224130793472';
+var briID = '170213128544452609';
+var retracksID = '224307245112885249';
+
+function isAdmin(userID) {
+    return (userID == quantumID) || (userID = briID);
+}
+
+function isDev(userID) {
+    return (userID == quantumID);
+}
+
+function isSelf(userID) {
+    return (userID == selfID);
+}
+
+function isHydraChannel(channelID) {
+    return (channelID == hydraChannel);
+}
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+    msgOp.post(bot, '629865351135363092', "Back alive... hopefully longer than the last time");
+});
+
+bot.on('disconnect', function(errMsg, code) {
+    msgOp.post(bot, '629865351135363092', "Aight imma head out");
+    console.log(new Date(), 'disconnected', errMsg, code);
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message.length <= 1)
+        return;
+    
+    if (isSelf(userID))
+        return;
+    
+    if (message.substring(0, 1) == ';') {        
+        // If textbender is on a break, don't do anything (except for media events)
+        if (actDumb && !isAdmin(userID))
+            return;
+        
+        // Check for the appropriate command type
+        var cmd = msgOp.getCmd(message);
+
+        // Bruh wyd, no command?
+        if (cmd == null) {
+            msgOp.nonsense(bot, channelID);
+            return;
+        }
+
+        // Blacklist retrack if annoying, handle that here
+        if (blacklistRetrack && isAdmin(userID)) {
+            msgOp.blacklisted(bot, channelID, userID);
+            return;
+        }
+        
+        cmd = cmd.toLowerCase();
+
+        // Main switch to control command flow (Grouped and then sorted alphabetically -> Length before case)
+        switch(cmd) {
+            // Utility commands
+            case 'help':
+                if (isAdmin(userID))
+                    msgOp.sendHelp(bot, evt);
+                else
+                    msgOp.helpResponder(bot, userID);
+                break;
+                
+            case 'helpme':
+                msgOp.helpResponder(bot, userID);
+                break;
+                
+            case 'ping':
+                msgOp.pingResponder(bot, channelID);
+                break;
+                
+            // Meme commands (pretty much)
+            case 'aight':
+                msgOp.post(bot, channelID, "<@" + userID + "> - Aight, imma head out");
+                break;
+                
+            case 'bruh':
+                msgOp.post(bot, channelID, "***B R U H*** *moment*");
+                msgOp.postBruh(bot, userID); 
+                break;
+                
+            case 'f':
+                msgOp.postF(bot, channelID, 'F');
+                break;
+                
+            case 'F':
+                msgOp.postF(bot, channelID, ':f_:');
+                break;
+                
+            case 'fuck':
+                msgOp.post(bot, channelID, "you.");
+                break;
+                
+            case 'killme':
+                msgOp.killResponder(bot, channelID, userID);
+                break;
+                
+            case 'mood':
+                msgOp.post(bot, channelID, "Damn you know shits real when someone hit you with mood");
+                break;
+                
+            case 'muda':
+                msgOp.postMuda(bot, channelID);
+                break;
+
+            case 'nice':
+                msgOp.postNice(bot, userID);
+                break;
+
+            case 'ora':
+                msgOp.postOra(bot, channelID);
+                break;
+                
+            case 'quoteme':
+                msgOp.postQuote(bot, message, userID);
+                break;
+                
+            case 'tananana':
+                msgOp.post(bot, channelID, "-*gasps*- Tu doo doo - doo du doo");
+                break;
+                
+            // Text's kitchen commands
+            case 'inv':
+                var invString = bend.getInvertedCaseString(msgOp.getArg(message, 4));
+                msgOp.post(bot, channelID, invString);
+                break;
+                
+            case 'invl':
+                msgOp.postLastMsgCased(bot, evt, bend, 2);
+                break;
+                
+            case 'low':
+                var lowString = bend.getLowerCaseString(msgOp.getArg(message, 4));
+                msgOp.post(bot, channelID, lowString);
+                break;
+                
+            case 'lowl':
+                msgOp.postLastMsgCased(bot, evt, bend, 0);
+                break;
+                
+            case 'mock':
+                var spongeBobString = bend.getSpongebobString(msgOp.getArg(message, 5));
+                if (spongeBobString == null)
+                    return msgOp.nonsense(bot, channelID);
+
+                msgOp.post(bot, channelID, spongeBobString);
+                break;
+
+            case 'mockl':
+            case 'mocklast':
+                msgOp.postLastMsgMocked(bot, evt, bend);
+                break;
+                
+            case 'space':
+            case 'spacew':
+            case 'biggspace':
+            case 'biggspacew':
+                msgOp.postMsgSpaced(bot, channelID, bend, cmd, message);
+                break;
+                
+            case 'spacel':
+            case 'biggspacel':
+                msgOp.postLastMsgSpaced(bot, evt, bend, cmd);
+                break;
+                
+            case 'upp':
+                var uppString = bend.getUpperCaseString(msgOp.getArg(message, 4));
+                msgOp.post(bot, channelID, uppString);
+                break;
+                
+            case 'uppl':
+                msgOp.postLastMsgCased(bot, evt, bend, 1);
+                break;
+
+            // Admin/Dev level commands:
+            case 'blacklistRetrancc':
+            case 'blacklistRetracc':
+            case 'blacklistRetrack':
+            case 'blacklistRent':
+            case 'blr':
+                if (isAdmin(userID))
+                    blacklistRetrack = !blacklistRetrack;
+                msgOp.post(bot, channelID, "Retracks blacklisted? " + blacklistRetrack);
+                break;
+                
+            case 'getcid':
+                if (isDev(userID))
+                    msgOp.post(bot, userID, channelID);
+                break;
+                
+            case 'id':
+                if (isDev(userID))
+                    msgOp.post(bot, userID, message.split('@')[1]);
+                break;
+                
+            case 'spam':
+                if (isDev(userID))
+                    msgOp.spamPost(bot, channelID, msgOp.getArg(message, 5));
+                else
+                    msgOp.weakSpamPost(bot, channelID);
+                break;
+                
+            case 'takeabreak':
+                if (isAdmin(userID)) {
+                    actDumb = true;
+                    msgOp.post(bot, channelID, "Aight Imma go have lunch. Tesla makes real nice batteries ;);)");
+                }
+                break;
+                
+            case 'wakeupdawg':
+                if (isAdmin(userID)) {
+                    actDumb = false;
+                    msgOp.post(bot, channelID, "Bet");
+                }
+                break;
+
+            default:
+                msgOp.nonsense(bot, channelID, userID);
+         }
+     }
+});
